f1768be69d43a7db387263da7b494e8aca7585fd
Gauree Shrikrishna Kale <gauree.kale@pratititech.com>
2025-08-01 17:50:44 +0530

    Microservice project setup

---
+++ Inventory App - Microservices/product-service/src/main/java/model/Product.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+
+@Document(value="product")
+@AllArgsConstructor
+
+public class Product {
+	
+
+}
